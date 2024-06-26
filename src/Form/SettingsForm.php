48da6c9049fd08fc4ede1f08ab8c31ebb83e6629
kouwa stephane <kksasteph888@gmail.com>
2024-06-26 07:46:26 +0200

    Maj

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -45,27 +45,27 @@ class SettingsForm extends ConfigFormBase {
       '#title' => $this->t('Texte &#039;add to cart&#039;'),
       '#maxlength' => 250,
       '#size' => 64,
-      '#default_value' => !empty($configs['commerce']['texte_add_to_cart']) ? $configs['commerce']['texte_add_to_cart'] : 'Add to cart'
+      '#default_value' => isset($configs['commerce']['texte_add_to_cart']) ? $configs['commerce']['texte_add_to_cart'] : 'Add to cart'
     ];
     $form['commerce']['checkout_button_text'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Texte "Passer la commande"'),
       '#maxlength' => 250,
       '#size' => 64,
-      '#default_value' => !empty($configs['commerce']['checkout_button_text']) ? $configs['commerce']['checkout_button_text'] : 'To order'
+      '#default_value' => isset($configs['commerce']['checkout_button_text']) ? $configs['commerce']['checkout_button_text'] : 'To order'
     ];
     $form['commerce']['cart_button_text'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Texte "Voir le panier"'),
       '#maxlength' => 250,
       '#size' => 64,
-      '#default_value' => !empty($configs['commerce']['cart_button_text']) ? $configs['commerce']['cart_button_text'] : 'See cart'
+      '#default_value' => isset($configs['commerce']['cart_button_text']) ? $configs['commerce']['cart_button_text'] : 'See cart'
     ];
     
     $form['commerce_style'] = [
       '#type' => 'details',
       '#title' => 'Commerce CSS class',
-      '#open' => true,
+      '#open' => false,
       '#tree' => true
     ];
     $form['commerce_style']['css_add_to_cart'] = [
@@ -73,7 +73,53 @@ class SettingsForm extends ConfigFormBase {
       '#title' => $this->t('Css button add to cart'),
       '#maxlength' => 250,
       '#size' => 64,
-      '#default_value' => !empty($configs['commerce_style']['css_add_to_cart']) ? $configs['commerce_style']['css_add_to_cart'] : 'btn btn-primary'
+      '#default_value' => isset($configs['commerce_style']['css_add_to_cart']) ? $configs['commerce_style']['css_add_to_cart'] : 'btn btn-primary'
+    ];
+    $form['commerce_style']['css_container'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('css_container'),
+      '#maxlength' => 250,
+      '#size' => 64,
+      '#default_value' => isset($configs['commerce_style']['css_container']) ? $configs['commerce_style']['css_container'] : 'row no-gutters g-2 d-flex align-items-end'
+    ];
+    $form['commerce_style']['css_container_qty'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('css_container_qty'),
+      '#maxlength' => 250,
+      '#size' => 64,
+      '#default_value' => isset($configs['commerce_style']['css_container_qty']) ? $configs['commerce_style']['css_container_qty'] : 'col-3'
+    ];
+    $form['commerce_style']['css_container_add_to_cart'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('css_container_add_to_cart'),
+      '#maxlength' => 250,
+      '#size' => 64,
+      '#default_value' => isset($configs['commerce_style']['css_container_add_to_cart']) ? $configs['commerce_style']['css_container_add_to_cart'] : 'col'
+    ];
+    $form['commerce_by_now'] = [
+      '#type' => 'details',
+      '#title' => 'commerce_by_now',
+      '#open' => false,
+      '#tree' => true
+    ];
+    $form['commerce_by_now']['enable'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('enable'),
+      '#default_value' => isset($configs['commerce_by_now']['enable']) ? $configs['commerce_by_now']['enable'] : false
+    ];
+    $form['commerce_by_now']['text'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('text'),
+      '#maxlength' => 250,
+      '#size' => 64,
+      '#default_value' => isset($configs['commerce_by_now']['text']) ? $configs['commerce_by_now']['text'] : 'Buy Now'
+    ];
+    $form['commerce_by_now']['class'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Class'),
+      '#maxlength' => 250,
+      '#size' => 64,
+      '#default_value' => isset($configs['commerce_by_now']['class']) ? $configs['commerce_by_now']['class'] : 'btn btn-outline-secondary'
     ];
     return parent::buildForm($form, $form_state);
   }
@@ -94,8 +140,8 @@ class SettingsForm extends ConfigFormBase {
     $config = $this->config('commerceformatage.settings');
     $config->set('commerce', $form_state->getValue('commerce'));
     $config->set('commerce_style', $form_state->getValue('commerce_style'));
+    $config->set('commerce_by_now', $form_state->getValue('commerce_by_now'));
     $config->save();
     parent::submitForm($form, $form_state);
   }
-  
 }

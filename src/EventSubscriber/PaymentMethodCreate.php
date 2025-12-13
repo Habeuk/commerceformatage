598aae4e2a5b880e31486d1b0c04d81ef5c5559c
kouwa stephane <kksasteph888@gmail.com>
2025-12-13 23:23:48 +0100

    amelioration de la config

--- src/EventSubscriber/PaymentMethodCreate.php
+++ src/EventSubscriber/PaymentMethodCreate.php
@@ -4,7 +4,6 @@ namespace Drupal\commerceformatage\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Drupal\commerce_stripe\Event\StripeEvents;
-use Drupal\commerce_stripe\Event\PaymentMethodCreateEvent;
 
 class PaymentMethodCreate implements EventSubscriberInterface {
   /**
@@ -13,22 +12,23 @@ class PaymentMethodCreate implements EventSubscriberInterface {
    * @var \Drupal\commerce_payment\Entity\PaymentMethodInterface
    */
   protected $paymentMethod;
-  
+
   function CreatePaymentMethode($event) {
-    \Drupal::messenger()->addStatus(" Run event subscriber ");
+    \Drupal::messenger()->addStatus(" commerceformatage::Run event subscriber ");
+    \Stephane888\Debug\debugLog::kintDebugDrupal($event, 'CreatePaymentMethode', true);
     $this->paymentMethod = $event->getPaymentMethod();
     // On desactive la reutilisation de la methode de paiement.
     $this->paymentMethod->setReusable(FALSE);
     // dd($paymentMethod);
   }
-  
+
   /**
    *
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
     return [
-      StripeEvents::PAYMENT_METHOD_CREATE => [
+      StripeEvents::PAYMENT_INTENT_CREATE => [
         'CreatePaymentMethode'
       ]
     ];
--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -6,7 +6,7 @@ use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Configure commerce formatage settings for this site.
+ * Configure Commerce Formatage settings.
  */
 class SettingsForm extends ConfigFormBase {
   
@@ -33,114 +33,131 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $configs = $this->config("commerceformatage.settings")->getRawData();
+    $configs = $this->config('commerceformatage.settings')->getRawData();
+    
     $form['commerce'] = [
       '#type' => 'details',
-      '#title' => 'Commerce configs',
+      '#title' => $this->t('Commerce button labels'),
+      '#description' => $this->t('Configure the text displayed on Commerce action buttons.'),
       '#open' => true,
       '#tree' => true
     ];
-    $form['commerce']['texte_add_to_cart'] = [
+    
+    $form['commerce']['text_add_to_cart'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Texte &#039;add to cart&#039;'),
+      '#title' => $this->t('Add to cart button label'),
+      '#description' => $this->t('Text displayed on the "Add to cart" button on product pages.'),
       '#maxlength' => 250,
       '#size' => 64,
-      '#default_value' => isset($configs['commerce']['texte_add_to_cart']) ? $configs['commerce']['texte_add_to_cart'] : 'Add to cart'
+      '#default_value' => $configs['commerce']['text_add_to_cart'] ?? 'Add to cart'
     ];
+    
     $form['commerce']['checkout_button_text'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Texte "Passer la commande"'),
+      '#title' => $this->t('Checkout button label'),
+      '#description' => $this->t('Text displayed on the checkout button.'),
       '#maxlength' => 250,
       '#size' => 64,
-      '#default_value' => isset($configs['commerce']['checkout_button_text']) ? $configs['commerce']['checkout_button_text'] : 'To order'
+      '#default_value' => $configs['commerce']['checkout_button_text'] ?? 'Checkout'
     ];
+    
     $form['commerce']['cart_button_text'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Texte "Voir le panier"'),
+      '#title' => $this->t('View cart button label'),
+      '#description' => $this->t('Text displayed on the "View cart" button.'),
       '#maxlength' => 250,
       '#size' => 64,
-      '#default_value' => isset($configs['commerce']['cart_button_text']) ? $configs['commerce']['cart_button_text'] : 'See cart'
+      '#default_value' => $configs['commerce']['cart_button_text'] ?? 'View cart'
     ];
     
     $form['commerce_style'] = [
       '#type' => 'details',
-      '#title' => 'Commerce CSS class',
+      '#title' => $this->t('Commerce CSS classes'),
+      '#description' => $this->t('Define CSS classes applied to Commerce elements.'),
       '#open' => false,
       '#tree' => true
     ];
+    
     $form['commerce_style']['css_add_to_cart'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Css button add to cart'),
+      '#title' => $this->t('Add to cart button CSS classes'),
+      '#description' => $this->t('CSS classes applied to the "Add to cart" button.'),
       '#maxlength' => 250,
       '#size' => 64,
-      '#default_value' => isset($configs['commerce_style']['css_add_to_cart']) ? $configs['commerce_style']['css_add_to_cart'] : 'btn btn-primary'
+      '#default_value' => $configs['commerce_style']['css_add_to_cart'] ?? 'btn btn-primary'
     ];
+    
     $form['commerce_style']['css_container'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('css_container'),
+      '#title' => $this->t('Buttons container CSS classes'),
+      '#description' => $this->t('CSS classes applied to the main buttons wrapper.'),
       '#maxlength' => 250,
       '#size' => 64,
-      '#default_value' => isset($configs['commerce_style']['css_container']) ? $configs['commerce_style']['css_container'] : 'row no-gutters g-2 d-flex align-items-end'
+      '#default_value' => $configs['commerce_style']['css_container'] ?? 'row g-2 align-items-end'
     ];
+    
     $form['commerce_style']['css_container_qty'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('css_container_qty'),
+      '#title' => $this->t('Quantity container CSS classes'),
+      '#description' => $this->t('CSS classes applied to the quantity field container.'),
       '#maxlength' => 250,
       '#size' => 64,
-      '#default_value' => isset($configs['commerce_style']['css_container_qty']) ? $configs['commerce_style']['css_container_qty'] : 'col-3'
+      '#default_value' => $configs['commerce_style']['css_container_qty'] ?? 'col-3'
     ];
+    
     $form['commerce_style']['css_container_add_to_cart'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('css_container_add_to_cart'),
+      '#title' => $this->t('Add to cart container CSS classes'),
+      '#description' => $this->t('CSS classes applied to the add-to-cart button container.'),
       '#maxlength' => 250,
       '#size' => 64,
-      '#default_value' => isset($configs['commerce_style']['css_container_add_to_cart']) ? $configs['commerce_style']['css_container_add_to_cart'] : 'col'
+      '#default_value' => $configs['commerce_style']['css_container_add_to_cart'] ?? 'col'
     ];
-    $form['commerce_by_now'] = [
+    
+    $form['commerce_buy_now'] = [
       '#type' => 'details',
-      '#title' => 'commerce_by_now',
+      '#title' => $this->t('Buy Now button'),
+      '#description' => $this->t('Configure the direct purchase (Buy Now) button.'),
       '#open' => false,
       '#tree' => true
     ];
-    $form['commerce_by_now']['enable'] = [
+    
+    $form['commerce_buy_now']['enable'] = [
       '#type' => 'checkbox',
-      '#title' => $this->t('enable'),
-      '#default_value' => isset($configs['commerce_by_now']['enable']) ? $configs['commerce_by_now']['enable'] : false
+      '#title' => $this->t('Enable Buy Now button'),
+      '#description' => $this->t('Enable a direct purchase button that skips the cart.'),
+      '#default_value' => $configs['commerce_buy_now']['enable'] ?? FALSE
     ];
-    $form['commerce_by_now']['text'] = [
+    
+    $form['commerce_buy_now']['text'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('text'),
+      '#title' => $this->t('Buy Now button label'),
+      '#description' => $this->t('Text displayed on the Buy Now button.'),
       '#maxlength' => 250,
       '#size' => 64,
-      '#default_value' => isset($configs['commerce_by_now']['text']) ? $configs['commerce_by_now']['text'] : 'Buy Now'
+      '#default_value' => $configs['commerce_buy_now']['text'] ?? 'Buy Now'
     ];
-    $form['commerce_by_now']['class'] = [
+    
+    $form['commerce_buy_now']['class'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Class'),
+      '#title' => $this->t('Buy Now button CSS classes'),
+      '#description' => $this->t('CSS classes applied to the Buy Now button.'),
       '#maxlength' => 250,
       '#size' => 64,
-      '#default_value' => isset($configs['commerce_by_now']['class']) ? $configs['commerce_by_now']['class'] : 'btn btn-outline-secondary'
+      '#default_value' => $configs['commerce_buy_now']['class'] ?? 'btn btn-outline-secondary'
     ];
+    
     return parent::buildForm($form, $form_state);
   }
   
   /**
-   *
-   * {@inheritdoc}
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    //
-  }
-  
-  /**
-   *
-   * {@inheritdoc}
+   * * * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->config('commerceformatage.settings');
     $config->set('commerce', $form_state->getValue('commerce'));
     $config->set('commerce_style', $form_state->getValue('commerce_style'));
-    $config->set('commerce_by_now', $form_state->getValue('commerce_by_now'));
+    $config->set('commerce_buy_now', $form_state->getValue('commerce_by_now'));
     $config->save();
     parent::submitForm($form, $form_state);
   }
